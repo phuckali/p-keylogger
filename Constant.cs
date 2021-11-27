3736afb48fc03fe24de9ba737d9ea95fae5c958c
Phuc Kali Team <95119776+phuckali@users.noreply.github.com>
2021-11-27 17:27:17 +0700

    Constant.cs

---
+++ Constant.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Visafe
+{
+    class Constant
+    {
+        public const string GET_DEVICE_ID_API = "https://app.visafe.vn/api/v1/control/gen-device-id";
+        public const string VERSION_INFO_URL = "https://raw.githubusercontent.com/Visafe/VisafeWindows/main/version_info.json";
+        public const string URL_CONFIG_FILE = "urlconfig.conf";
+        public const string USERID_CONFIG_FILE = "userid.conf";
+        public const string VERSION_FILE_NAME = "version.txt";
+        public const string NOTI_TITLE = "VISAFE";
+        public const int NOTI_TIME = 500;
+        public const string DNS_BASE_URL = "dns.visafe.vn";
+        public const string ADMIN_DASHBOARD_URL = "https://app.visafe.vn";
+        public const string VISAFE_DOC_URL = "https://docs.visafe.vn";
+        public const string SAVING_ERROR_MSG = "KHÔNG THỂ LƯU URL";
+        public const string SAVING_SUCCESS_MSG = "Đã lưu URL";
+        public const string STARTED_NOTI_STRING = "started";
+        public const string VISAFE_SERVICE_PIPE = "VisafeServicePipe";
+    }
+}
